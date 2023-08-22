d7fcfb66dd5f10763d88f400f2c3f8818d3d4658
rizkyilhampra <rizkyilhamp16@gmail.com>
2023-08-22 20:55:24 +0800

    create migration, model, seeder address and add relation

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Contact extends Model
 {
@@ -16,5 +17,10 @@ class Contact extends Model
     public function users(): BelongsTo
     {
         return $this->belongsTo(Contact::class, 'user_id', 'id');
+    }
+
+    public function addresses(): HasMany
+    {
+        return $this->hasMany(Address::class, 'contact_id', 'id');
     }
 }
