ad7d7809bf3ed6dcc010c66910ca46f6601651d2
rizkyilhampra <rizkyilhamp16@gmail.com>
2023-08-22 20:27:36 +0800

    create model migration seeder user

--- database/migrations/2023_08_22_122120_create_users_table.php
+++ database/migrations/2023_08_22_122120_create_users_table.php
@@ -13,11 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('username', 50)->unique()->nullable(false)->unique("users_username_unique");
+            $table->string('password', 255)->nullable(false);
+            $table->string('name', 100)->nullable('false');
+            $table->string('token', 255)->nullable();
             $table->timestamps();
         });
     }
